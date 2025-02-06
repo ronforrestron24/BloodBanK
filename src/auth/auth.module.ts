82e5edf8d7d5d159e1b1e31c22d56878a4492a0c
Ron Manguerra <ronforrestron24@gmail.com>
2025-02-06 21:51:10 +0800

    feat: update

--- src/auth/auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthResolver } from './auth.resolver';
-
-@Module({
-  controllers: [AuthController],
-  providers: [AuthResolver]
-})
-export class AuthModule {}
